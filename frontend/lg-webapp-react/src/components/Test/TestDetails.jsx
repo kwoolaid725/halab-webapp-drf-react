4bc461fd796d41b98bd44418627eb829486a519f
Woohyun Kim <woodenkim92@gmail.com>
2023-11-25 16:13:13 -0600

    test table

--- frontend/lg-webapp-react/src/components/Test/TestDetails.jsx
+++ frontend/lg-webapp-react/src/components/Test/TestDetails.jsx
@@ -1,151 +1,71 @@
-import { useState } from "react";
-
-// import "./App.css";
-import { TestDetailsTableRow } from "./TestDetailsTableRow";
-import  TestDetailsTable  from "./TestDetailsTable";
-import { TestDetailsHeader } from "./TestDetailsHeader";
-import { TestModal } from "./TestModal";
-
-const testMeasure = {
-  Bare: [
-      {
-      Sand: [
-        {
-          soil_weight: {
-              value: '40',
-              units: 'g'
-          },
-          vac_weight_i: {
-              value: '',
-              units: 'g'
-          },
-          vac_weight_f: {
-              value: '',
-              units: 'g'
-          },
-          vac_weight_diff: {
-              value: '',
-              units: 'g'
-          },
-          pickup: {
-              value: '',
-              units: '%'
-          }
-        }
-        ]
-      },
-      {
-        Rice: [
-          {
-            soil_weight: {
-              value: '40',
-              units: 'g'
-            },
-            vac_weight_i: {
-              value: '',
-              units: 'g'
-            },
-            vac_weight_f: {
-              value: '',
-              units: 'g'
-            },
-            vac_weight_diff: {
-              value: '',
-              units: 'g'
-            },
-            pickup: {
-              value: '',
-              units: '%'
-            }
-          }
-        ]
-      },
-      {
-        Cheerios: [
-          {
-            soil_weight: {
-                value: '40',
-                units: 'g'
-            },
-            vac_weight_i: {
-                value: '',
-                units: 'g'
-            },
-            vac_weight_f: {
-                value: '',
-                units: 'g'
-            },
-            vac_weight_diff: {
-                value: '',
-                units: 'g'
-            },
-            pickup: {
-                value: '',
-                units: '%'
-            }
-          }
-        ]
-      }
-  ]};
-function TestDetails({testTarget}) {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [rows, setRows] = useState([
-    testMeasure[testTarget].map(testGroup => ({
-      slug: '',
-      tester: '',
-      test_group: '',
-      test_measure: testMeasure[Object.keys(testGroup)[0]][0],
-      value: testMeasure[Object.keys(testGroup)[0]][0].value,
-      units: testMeasure[Object.keys(testGroup)[0]][0].units,
-      run: '',
-      remarks: '',
-      created: '',
-      updated: '',
-    }))
-
-  ]);
-  const [rowToEdit, setRowToEdit] = useState(null);
-
-  const handleDeleteRow = (targetIndex) => {
-    setRows(rows.filter((_, idx) => idx !== targetIndex));
-  };
-
-  const handleEditRow = (idx) => {
-    setRowToEdit(idx);
-
-    setModalOpen(true);
-  };
-
-  const handleSubmit = (newRow) => {
-    rowToEdit === null
-      ? setRows([...rows, newRow])
-      : setRows(
-          rows.map((currRow, idx) => {
-            if (idx !== rowToEdit) return currRow;
-
-            return newRow;
-          })
-        );
-  };
-
-  return (
-    <div className="App">
-      <TestDetailsTable rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
-      <button onClick={() => setModalOpen(true)} className="btn">
-        Add
-      </button>
-      {modalOpen && (
-        <TestModal
-          closeModal={() => {
-            setModalOpen(false);
-            setRowToEdit(null);
-          }}
-          onSubmit={handleSubmit}
-          defaultValue={rowToEdit !== null && rows[rowToEdit]}
-        />
-      )}
-    </div>
-  );
-}
-
-export default TestDetails;
+// import { useState } from "react";
+//
+// // import "./App.css";
+// import { TestDetailsTableRow } from "./TestDetailsTableRow";
+// import  TestDetailsTable  from "./TestDetailsTable";
+// import { TestDetailsHeader } from "./TestDetailsHeader";
+// import { TestModal } from "./TestModal";
+//
+//
+// function TestDetails({testTarget}) {
+//   const [modalOpen, setModalOpen] = useState(false);
+//   const [rows, setRows] = useState([
+//     testMeasure[testTarget].map(testGroup => ({
+//       slug: '',
+//       tester: '',
+//       test_group: '',
+//       test_measure: testMeasure[Object.keys(testGroup)[0]][0],
+//       value: testMeasure[Object.keys(testGroup)[0]][0].value,
+//       units: testMeasure[Object.keys(testGroup)[0]][0].units,
+//       run: '',
+//       remarks: '',
+//       created: '',
+//       updated: '',
+//     }))
+//
+//   ]);
+//   const [rowToEdit, setRowToEdit] = useState(null);
+//
+//   const handleDeleteRow = (targetIndex) => {
+//     setRows(rows.filter((_, idx) => idx !== targetIndex));
+//   };
+//
+//   const handleEditRow = (idx) => {
+//     setRowToEdit(idx);
+//
+//     setModalOpen(true);
+//   };
+//
+//   const handleSubmit = (newRow) => {
+//     rowToEdit === null
+//       ? setRows([...rows, newRow])
+//       : setRows(
+//           rows.map((currRow, idx) => {
+//             if (idx !== rowToEdit) return currRow;
+//
+//             return newRow;
+//           })
+//         );
+//   };
+//
+//   return (
+//     <div className="App">
+//       <TestDetailsTable rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+//       <button onClick={() => setModalOpen(true)} className="btn">
+//         Add
+//       </button>
+//       {modalOpen && (
+//         <TestModal
+//           closeModal={() => {
+//             setModalOpen(false);
+//             setRowToEdit(null);
+//           }}
+//           onSubmit={handleSubmit}
+//           defaultValue={rowToEdit !== null && rows[rowToEdit]}
+//         />
+//       )}
+//     </div>
+//   );
+// }
+//
+// export default TestDetails;
--- frontend/lg-webapp-react/src/components/Test/TestDetailsTableRow.jsx
+++ frontend/lg-webapp-react/src/components/Test/TestDetailsTableRow.jsx
@@ -1,14 +1,176 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useState
+} from 'react'
 import { BsFillTrashFill, BsFillPencilFill } from 'react-icons/bs';
-const TestDetailsTableRow = ({ rows, testGroup, testMeasures, addRow  }) => {
-  console.log('testMeasures:', testMeasures);
-  let keys = Object.keys(testMeasures);
-  let values = [];
-  for (let key in testMeasures) {
-    let value = testMeasures[key];
-    console.log(key, value);
-    values.push(<th>{value.value}</th>);
+import Button from '@mui/material/Button';
+
+
+// const testGroup = 'Bare';
+// const testMeasures = {
+//
+// }
+
+
+
+
+// const TestDetailsTableRow = ({  testGroup = '', testMeasures ={}, addRow, deleteRow, editRow }) => {
+const TestDetailsTableRow = ({  addRow, deleteRow, editRow }) => {
+  const [rows, setRows] = useState([
+    {
+      slug: '1',
+      tester: 'a',
+      testGroup: '',
+      // test_measure: '',
+      // value: '',
+      // units: '',
+      run: 1,
+      remarks: 'adf',
+      created_at: '',
+      last_updated: '',
+    }
+
+  ]);
+
+  // const testGroup = 'Bare';
+  const testMeasures = {
+  "Bare": [
+    {
+      "Sand": [
+        {
+          "soil_weight": {
+            "value": "40",
+            "units": "g"
+          },
+          "vac_weight_i": {
+            "value": "",
+            "units": "g"
+          },
+          "vac_weight_f": {
+            "value": "",
+            "units": "g"
+          },
+          "vac_weight_diff": {
+            "value": "",
+            "units": "g"
+          },
+          "pickup": {
+            "value": "",
+            "units": "%"
+          }
+        }
+      ]
+    },
+    {
+      "Rice": [
+        {
+          "soil_weight": {
+            "value": "40",
+            "units": "g"
+          },
+          "vac_weight_i": {
+            "value": "",
+            "units": "g"
+          },
+          "vac_weight_f": {
+            "value": "",
+            "units": "g"
+          },
+          "vac_weight_diff": {
+            "value": "",
+            "units": "g"
+          },
+          "pickup": {
+            "value": "",
+            "units": "%"
+          }
+        }
+      ]
+    },
+    {
+      "Cheerios": [
+        {
+          "soil_weight": {
+            "value": "40",
+            "units": "g"
+          },
+          "vac_weight_i": {
+            "value": "",
+            "units": "g"
+          },
+          "vac_weight_f": {
+            "value": "",
+            "units": "g"
+          },
+          "vac_weight_diff": {
+            "value": "",
+            "units": "g"
+          },
+          "pickup": {
+            "value": "",
+            "units": "%"
+          }
+        }
+      ]
+    }
+  ],
+  "Carpet":
+      {
+      "Sand":
+        {
+          "soil_weight": {
+              "value": "100",
+              "units": "g"
+          },
+          "vac_weight_i": {
+              "value": "",
+              "units": "g"
+          },
+          "vac_weight_f": {
+              "value": "",
+              "units": "g"
+          },
+          "vac_weight_diff": {
+              "value": "",
+              "units": "g"
+          },
+          "pickup": {
+              "value": "",
+              "units": "%"
+          }
+        }
+      }
+}
+
+const testGroup = 'Sand';
+
+  const handleAddRow = () => {
+    addRow(testGroup, testMeasures); // Pass test group and test target data to addRow function
+  };
+
+
+ // Check if testMeasures and testGroup are valid
+  if (!testMeasures || !testMeasures[testGroup]) {
+    return (
+      <div>
+        <p>Test measures for {testGroup} are not available.</p>
+      </div>
+    );
   }
+
+  // Extract keys and values from testMeasures[testGroup]
+  // let keys = [];
+  // let values = [];
+
+  // Extract keys and values from testMeasures[testGroup]
+  const measuresKeys = Object.keys(testMeasures[testGroup]); // Array of keys for the measures
+  const selectedMeasures = testMeasures[testGroup][measuresKeys[0]]; // Assuming taking the first instance
+
+  const keys = Object.keys(selectedMeasures[0]); // Assuming it's an array with at least one item
+  const values = keys.map((key) => selectedMeasures[0][key].value);
+
+  console.log('keys:', keys);
+  console.log('values:', values);
 
   return (
     <div>
@@ -18,7 +180,9 @@ const TestDetailsTableRow = ({ rows, testGroup, testMeasures, addRow  }) => {
             <th>ID</th>
             <th>Tester</th>
             <th>Test Group</th>
-            {keys}
+            {keys.map((key, idx) => (
+              <th key={idx}>{key}</th>
+            ))}
             <th>Run</th>
             <th>Remarks</th>
             <th>Created</th>
@@ -27,14 +191,15 @@ const TestDetailsTableRow = ({ rows, testGroup, testMeasures, addRow  }) => {
           </tr>
         </thead>
         <tbody>
-        {rows && rows.map((row, idx) =>  {
-          return (
-            <>
-              <tr key={idx}>
-                <td>{row.id}</td>
+          {rows.map((row, idx) => (
+            <React.Fragment key={idx}>
+              <tr>
+                <td>{row.slug}</td>
                 <td>{row.tester}</td>
                 <td>{testGroup}</td>
-                {values}
+                {values.map((value, index) => (
+                  <td key={index}>{value}</td>
+                ))}
                 <td>{row.run}</td>
                 <td>{row.remarks}</td>
                 <td>{row.created_at}</td>
@@ -46,19 +211,20 @@ const TestDetailsTableRow = ({ rows, testGroup, testMeasures, addRow  }) => {
                   </span>
                 </td>
               </tr>
-              <tr>
-                <td colSpan={9}>
-                  <button onClick={addRow}>Add</button>
-                </td>
-              </tr>
-            </>
-          );
-        })}
+            </React.Fragment>
+          ))}
+          <tr>
+            <td colSpan={keys.length + 7}>
+              <Button variant="contained" onClick={handleAddRow}>
+                Add Row
+              </Button>
+            </td>
+          </tr>
         </tbody>
       </table>
     </div>
   );
-}
+};
 
 export default TestDetailsTableRow;
 
